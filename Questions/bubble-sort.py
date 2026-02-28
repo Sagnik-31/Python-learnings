a393e62011f512ee4551277dd859484f424952dd
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Air.local>
2026-02-28 23:54:28 +0530

    bubble sort

---
+++ Questions/bubble-sort.py
@@ -0,0 +1,9 @@
+nums = [5,1,6,8,2,4,9]
+n = len(nums)
+
+for i in range(n-2,-1,-1):
+    for j in range(0,i+1):
+        if nums[j]>nums[j+1]:
+            nums[j],nums[j+1] = nums[j+1],nums[j]
+
+print(nums)
